b1ce58bf95448275e0f9fc2c9814cb8f2717174d
Ben Koala <bkoala123@hotmail.com>
2021-11-06 19:24:02 -0700

    Updated Readme

--- bk_react_portofolio/src/components/Portofolio.js
+++ bk_react_portofolio/src/components/Portofolio.js
@@ -3,6 +3,8 @@ import Project1 from '../images/project-1.png'
 import Project2 from '../images/project-2.png'
 import Weather from '../images/weather.png'
 import Tracker from '../images/Tracker.png'
+import Scheduler from '../images/scheduler.png'
+import Css1 from '../images/css1.png'
 
 export const Portofolio = () => {
     return (
@@ -40,6 +42,22 @@ export const Portofolio = () => {
                 </div>
              </div>
         </section>
+        <section  class="grid-container">
+           <div  class="grid-item">
+           <div class="row">
+                <a href="https://bkoala.github.io/Css_style/"> 
+                <img src={Css1} alt="Css style" /></a>     
+            </div>
+            </div>
+            <div  class="grid-item">
+                <div class="row">
+                <a href="https://bkoala.github.io/Bkworkday_scheduler/">                                 
+                <img src={Scheduler} alt="Scheduler " />
+                 </a>
+                </div>
+             </div>
+        </section>
+
        </article>
 </div>
     )
--- bk_react_portofolio/src/components/Socialmedia.js
+++ bk_react_portofolio/src/components/Socialmedia.js
@@ -6,8 +6,8 @@ export const Socialmedia = () => {
 <section >
         <div >
         <div className="social-icons-container">
-            <SocialIcon className="social-icon" target="_blank" url="https://twitter.com" />
-            <SocialIcon className="social-icon" target="_blank" url="https://instagram.com" />
+        <SocialIcon className="social-icon" target="_blank" url="https://www.github.com/bkoala" />
+            <SocialIcon className="social-icon" target="_blank" url="https://www.linkedin.com/in/ben-k-5918917/" />
             <SocialIcon className="social-icon" target="_blank" url="https://facebook.com" />
             <SocialIcon className="social-icon" target="_blank" url="https://reddit.com" />
         </div>
